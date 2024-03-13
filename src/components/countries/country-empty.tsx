fef9c4c3d7225b3959556dc354d89467c987ca5c
Rachhen <rachhen.it@outlook.com>
2024-03-13 20:04:19 +0700

    added country not found

---
+++ src/components/countries/country-empty.tsx
@@ -0,0 +1,9 @@
+export function CountryEmpty() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <h3 className="text-xl text-foreground/70 font-semibold">
+        Not country found
+      </h3>
+    </div>
+  );
+}
--- src/components/countries/country-list.tsx
+++ src/components/countries/country-list.tsx
@@ -1,8 +1,13 @@
 import { useCountryContext } from "~/contexts/country";
 import { CountryItem } from "./country-item";
+import { CountryEmpty } from "./country-empty";
 
 export default function CountryList() {
   const { countries } = useCountryContext();
+
+  if (countries.length === 0) {
+    return <CountryEmpty />;
+  }
 
   return (
     <div className="grid grid-cols-5 gap-4">
